41fadc9b58b8f06c1241c8802401e3d3c7c6f872
banksjazz87@gmail.com <banksjazz87@gmail.com>
2021-10-25 05:24:50 -0400

    chore: I'm pushing everything I have so far.

---
+++ hide.js
@@ -0,0 +1,6 @@
+let carsXeApiKey = "4jrklhq7m_gf7qnq5f5_oty30rgzz";
+http://api.carsxe.com/specs?key=4jrklhq7m_gf7qnq5f5_oty30rgzz&/id/448
+
+https://vpic.nhtsa.dot.gov/api/vehicles/getmanufacturerdetails/honda?format=json
+
+https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeYear/make/toyota/modelyear/2015?format=json
